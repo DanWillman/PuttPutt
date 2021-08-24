fded3ad6620f435bcf1566b6229dc2e2f6928f6f
Dan Willman <github@danwillman.com>
2021-08-23 22:27:50 -0500

    placeholder for commands

--- Commands/BasicCommands.cs
+++ Commands/BasicCommands.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
-
 namespace PuttPutt.Commands
 {
     public class BasicCommands : BaseCommandModule
     {
-        
+        [Command("sync")]
+        [Hidden]
+        //[RequireRoles(RoleCheckMode.All, "admin")]
+        public async Task SyncScores(CommandContext ctx)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
