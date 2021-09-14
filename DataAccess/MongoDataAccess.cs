fa3a88d7775c4fcf870b0cd13a1f295b1143e652
Dan Willman <RikerCabra@gmail.com>
2021-09-14 18:52:01 -0500

    Season ending stuff

--- DataAccess/MongoDataAccess.cs
+++ DataAccess/MongoDataAccess.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using DSharpPlus.Entities;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Newtonsoft.Json;
 using PuttPutt.Models;
 
 namespace PuttPutt.DataAccess
@@ -12,25 +13,30 @@ namespace PuttPutt.DataAccess
     /// </summary>
     public class MongoDataAccess
     {
-        private readonly string CollectionName = "scores";
+        private readonly string ParticipantCollectionName = "scores";
+        private readonly string ArchiveCollectionName = "archive";
         private readonly string ConnectionString = "mongodb://192.168.1.69:27018";
         private readonly string DatabaseName = "shame_golf";
 
-        private readonly IMongoCollection<Participant> collection;
+        private readonly IMongoCollection<Participant> particpantCollection;
+        private readonly IMongoCollection<Archive> archiveCollection;
 
         public MongoDataAccess()
         {
             var client = new MongoClient(ConnectionString);
             var database = client.GetDatabase(DatabaseName);
 
-            collection = database.GetCollection<Participant>(CollectionName);
-            collection.Indexes.CreateOne(new CreateIndexModel<Participant>(Builders<Participant>.IndexKeys.Ascending(x => x.UserId)));
+            particpantCollection = database.GetCollection<Participant>(ParticipantCollectionName);
+            particpantCollection.Indexes.CreateOne(new CreateIndexModel<Participant>(Builders<Participant>.IndexKeys.Ascending(x => x.UserId)));
+
+            archiveCollection = database.GetCollection<Archive>(ArchiveCollectionName);
+            archiveCollection.Indexes.CreateOne(new CreateIndexModel<Archive>(Builders<Archive>.IndexKeys.Ascending(x => x.Id)));
         }
 
         /// <summary>
         /// Returns an unsorted list of all particpant entries for a specified server
         /// </summary>
-        public List<Participant> GetParticipants(DiscordGuild server) => collection.Find(x => x.ServerId == server.Id).ToList();
+        public List<Participant> GetParticipants(DiscordGuild server) => particpantCollection.Find(x => x.ServerId == server.Id).ToList();
 
         /// <summary>
         /// Get participant info for a specified user/server combination
@@ -39,7 +45,7 @@ namespace PuttPutt.DataAccess
         /// <param name="server">DiscordGuild (server) tied to record</param>
         /// <returns></returns>
         public Participant GetParticipantInfo(DiscordUser user, DiscordGuild server) =>
-            collection.Find(x => x.UserId == user.Id && x.ServerId == server.Id).FirstOrDefault();
+            particpantCollection.Find(x => x.UserId == user.Id && x.ServerId == server.Id).FirstOrDefault();
 
         /// <summary>
         /// Updates the participant info, inserting if missing.
@@ -53,7 +59,7 @@ namespace PuttPutt.DataAccess
                 participant.Id = ObjectId.GenerateNewId().ToString();
             }
 
-            collection.ReplaceOne(x => x.UserId == participant.UserId && x.ServerId == participant.ServerId, participant, new ReplaceOptions() { IsUpsert = true });
+            particpantCollection.ReplaceOne(x => x.UserId == participant.UserId && x.ServerId == participant.ServerId, participant, new ReplaceOptions() { IsUpsert = true });
 
             return GetParticipantInfo(participant);
         }
@@ -62,6 +68,35 @@ namespace PuttPutt.DataAccess
         /// Helper method fetches participant by object
         /// </summary>
         private Participant GetParticipantInfo(Participant participant) =>
-            collection.Find(x => x.UserId == participant.UserId && x.ServerId == participant.ServerId).FirstOrDefault();
+            particpantCollection.Find(x => x.UserId == participant.UserId && x.ServerId == participant.ServerId).FirstOrDefault();
+
+        /// <summary>
+        /// Gets all archive names from the specified server
+        /// </summary>
+        /// <param name="server"></param>
+        /// <returns></returns>
+        public List<string> GetArchivalNames(DiscordGuild server) => archiveCollection.Find(x => x.ServerId == server.Id).ToList().Select(a => a.ArchiveName).Distinct().ToList();
+
+        /// <summary>
+        /// Moves records from current 
+        /// </summary>
+        /// <param name="server">Discord server for season</param>
+        /// <param name="archiveName">Archival name given to season</param>
+        public void ArchiveSeason(DiscordGuild server, string archiveName)
+        {
+            var archive = new Archive()
+            {
+                ArchiveName = archiveName,
+                Id = ObjectId.GenerateNewId().ToString(),
+                ServerId = server.Id,
+                Participant = GetParticipants(server)
+            };
+
+            System.Console.WriteLine($"Archiving season. {JsonConvert.SerializeObject(archive)}");
+
+            archiveCollection.InsertOne(archive);
+
+            particpantCollection.DeleteMany(x => x.ServerId == server.Id);
+        }
     }
 }
