48b00686556dfb856a8cb9d3c652f1ee56a43e6f
Dan Willman <RikerCabra@gmail.com>
2021-09-19 12:56:32 -0500

    move mongo to interface

---
+++ src/DataAccess/IMongoDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using DSharpPlus.Entities;
+using PuttPutt.Models;
+using System.Collections.Generic;
+
+namespace PuttPutt.DataAccess
+{
+    /// <summary>
+    /// Provides access to underlying data store in Mongo
+    /// </summary>
+    public interface IMongoDataAccess
+    {
+        /// <summary>
+        /// Moves records from scores collection to a named archive collection entry
+        /// </summary>
+        /// <param name="server">Discord server for season</param>
+        /// <param name="archiveName">Archival name given to season</param>
+        void ArchiveSeason(DiscordGuild server, string archiveName);
+
+        /// <summary>
+        /// Gets all archive names from the specified server
+        /// </summary>
+        /// <param name="server"></param>
+        /// <returns></returns>
+        List<string> GetArchivalNames(DiscordGuild server);
+
+        /// <summary>
+        /// Gets the archival entry for the specified server and name
+        /// </summary>
+        /// <param name="server"></param>
+        /// <param name="archiveName"></param>
+        /// <returns></returns>
+        Archive GetArchive(DiscordGuild server, string archiveName);
+
+        /// <summary>
+        /// Get participant info for a specified user/server combination
+        /// </summary>
+        /// <param name="user">DiscordUser being searched for</param>
+        /// <param name="server">DiscordGuild (server) tied to record</param>
+        /// <returns></returns>
+        Participant GetParticipantInfo(DiscordUser user, DiscordGuild server);
+
+        Participant GetParticipantInfo(ulong userId, ulong serverId);
+
+        /// <summary>
+        /// Returns an unsorted list of all particpant entries for a specified server
+        /// </summary>
+        List<Participant> GetParticipants(DiscordGuild server);
+
+        /// <summary>
+        /// Updates the participant info, inserting if missing.
+        /// <para/>
+        /// Returns updated participant value
+        /// </summary>
+        Participant UpsertParticipant(Participant participant);
+    }
+}
--- src/DataAccess/MongoDataAccess.cs
+++ src/DataAccess/MongoDataAccess.cs
@@ -9,10 +9,8 @@ using PuttPutt.Models;
 
 namespace PuttPutt.DataAccess
 {
-    /// <summary>
-    /// Provides access to underlying data store in Mongo
-    /// </summary>
-    public class MongoDataAccess
+    /// <inheritdoc/>
+    public class MongoDataAccess : IMongoDataAccess
     {
         private readonly string ParticipantCollectionName = "scores";
         private readonly string ArchiveCollectionName = "archive";
@@ -22,6 +20,9 @@ namespace PuttPutt.DataAccess
         private readonly IMongoCollection<Participant> particpantCollection;
         private readonly IMongoCollection<Archive> archiveCollection;
 
+        /// <summary>
+        /// IoC constructor
+        /// </summary>
         public MongoDataAccess()
         {
             var client = new MongoClient(ConnectionString);
@@ -34,25 +35,14 @@ namespace PuttPutt.DataAccess
             archiveCollection.Indexes.CreateOne(new CreateIndexModel<Archive>(Builders<Archive>.IndexKeys.Ascending(x => x.Id)));
         }
 
-        /// <summary>
-        /// Returns an unsorted list of all particpant entries for a specified server
-        /// </summary>
         public List<Participant> GetParticipants(DiscordGuild server) => particpantCollection.Find(x => x.ServerId == server.Id).ToList();
 
-        /// <summary>
-        /// Get participant info for a specified user/server combination
-        /// </summary>
-        /// <param name="user">DiscordUser being searched for</param>
-        /// <param name="server">DiscordGuild (server) tied to record</param>
-        /// <returns></returns>
         public Participant GetParticipantInfo(DiscordUser user, DiscordGuild server) =>
             particpantCollection.Find(x => x.UserId == user.Id && x.ServerId == server.Id).FirstOrDefault();
 
-        /// <summary>
-        /// Updates the participant info, inserting if missing.
-        /// <para/>
-        /// Returns updated participant value
-        /// </summary>
+        public Participant GetParticipantInfo(ulong userId, ulong serverId) =>
+            particpantCollection.Find(x => x.UserId == userId && x.ServerId == serverId).FirstOrDefault();
+
         public Participant UpsertParticipant(Participant participant)
         {
             if(string.IsNullOrWhiteSpace(participant.Id))
@@ -65,32 +55,10 @@ namespace PuttPutt.DataAccess
             return GetParticipantInfo(participant);
         }
 
-        /// <summary>
-        /// Helper method fetches participant by object
-        /// </summary>
-        private Participant GetParticipantInfo(Participant participant) =>
-            particpantCollection.Find(x => x.UserId == participant.UserId && x.ServerId == participant.ServerId).FirstOrDefault();
-
-        /// <summary>
-        /// Gets all archive names from the specified server
-        /// </summary>
-        /// <param name="server"></param>
-        /// <returns></returns>
         public List<string> GetArchivalNames(DiscordGuild server) => archiveCollection.Find(x => x.ServerId == server.Id).ToList().Select(a => a.ArchiveName).Distinct().ToList();
 
-        /// <summary>
-        /// Gets the archival entry for the specified server and name
-        /// </summary>
-        /// <param name="server"></param>
-        /// <param name="archiveName"></param>
-        /// <returns></returns>
         public Archive GetArchive(DiscordGuild server, string archiveName) => archiveCollection.Find(x => x.ServerId == server.Id && x.ArchiveName.Equals(archiveName)).FirstOrDefault();
 
-        /// <summary>
-        /// Moves records from current 
-        /// </summary>
-        /// <param name="server">Discord server for season</param>
-        /// <param name="archiveName">Archival name given to season</param>
         public void ArchiveSeason(DiscordGuild server, string archiveName)
         {
             var archive = new Archive()
@@ -101,11 +69,17 @@ namespace PuttPutt.DataAccess
                 Participant = GetParticipants(server)
             };
 
-            System.Console.WriteLine($"Archiving season. {JsonConvert.SerializeObject(archive)}");
+            Console.WriteLine($"Archiving season. {JsonConvert.SerializeObject(archive)}");
 
             archiveCollection.InsertOne(archive);
 
             particpantCollection.DeleteMany(x => x.ServerId == server.Id);
         }
+
+        /// <summary>
+        /// Helper method fetches participant by object
+        /// </summary>
+        private Participant GetParticipantInfo(Participant participant) =>
+            particpantCollection.Find(x => x.UserId == participant.UserId && x.ServerId == participant.ServerId).FirstOrDefault();
     }
 }
